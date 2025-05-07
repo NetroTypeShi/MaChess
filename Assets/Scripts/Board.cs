922cf7fd46a767c74c05675557704528ca662bec
NetroTypeShi <andresmenduina@gmail.com>
2025-05-07 12:48:46 +0200

    Trasspassing to other pc

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,38 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Manager : MonoBehaviour
+public class Board
 {
-    BoardSquare[,] squares;
-
-    [SerializeField] GameObject squarePrefab;
-    [SerializeField] int x = 8; // columnas
-    [SerializeField] int y = 8; // filas
-    [SerializeField] float spacing;
-    [SerializeField] Material whiteMaterial;
-    [SerializeField] Material blackMaterial;
-    GameObject square;
-    Vector3 position;
-    Vector2Int intPosition;
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        squares = new BoardSquare[x, y];
-        StartCoroutine(InstantiateCubeEdges());
+        
     }
 
-    IEnumerator InstantiateCubeEdges()
+    // Update is called once per frame
+    void Update()
     {
-        for (int i = 0; i < x; i++)
-        {
-            for (int j = 0; j < y; j++)
-            {
-                Vector2Int intPosition = new Vector2Int(i, j);
-                squares[i, j] = new BoardSquare(intPosition, squarePrefab, transform, whiteMaterial, blackMaterial);
-                yield return null;
-            }
-        }
-
-        yield return new WaitForEndOfFrame();
+        
     }
 }
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    BoardSquare[,] squares;
+
+    [SerializeField] GameObject squarePrefab;
+    [SerializeField] int x = 8; // columnas
+    [SerializeField] int y = 8; // filas
+    [SerializeField] float spacing;
+    [SerializeField] Material whiteMaterial;
+    [SerializeField] Material blackMaterial;
+    GameObject square;
+    Vector3 position;
+    Vector2Int intPosition;
+
+    private void Awake()
+    {
+        squares = new BoardSquare[x, y];
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                Vector2Int intPosition = new Vector2Int(i, j);
+                squares[i, j] = new BoardSquare(intPosition, squarePrefab, transform, whiteMaterial, blackMaterial);
+            }
+        }
+    }
+}
